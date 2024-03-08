f707f4d18db6ba9c62c0d9eaaaefb9f239c5049a
Darren <djamesscott90@gmail.com>
2024-03-08 22:21:54 +0000

    Added timing features

--- main.py
+++ main.py
@@ -3,13 +3,48 @@
 import numpy as np
 import network_construction as netcon
 from brute_force import brute_force
+#from plotting_routines import plot_timings
+import time
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+
+results_full = []
+with open('./output/timings.csv','w') as file:
+    writer = csv.writer(file)
+    fields = ['n_groups', 'n_rows', 'n_cols', 'first_time', 'avg_time']
+    writer.writerow(fields)
 
 
-rows = 2
-columns = 2
-n_groups = 3
-weights = netcon.build_test_network(rows, columns, n_groups)
+    for n_groups in range(3,11):
+        group_size = 3
+        rows = group_size
+        columns = group_size
+        #weights = netcon.build_test_network(rows, columns, n_groups)
+        #routes = brute_force(weights)
 
-routes = brute_force(weights)
-print("Routes")
-print(routes)
+        times = []
+        s_routes = []
+        for _ in range(10):
+            weights = netcon.build_test_network(rows, columns, n_groups)
+            start = time.perf_counter()
+            routes = brute_force(weights)
+            shortest = min(routes)
+            time_elapsed = time.perf_counter() - start
+            times.append(time_elapsed)
+            s_routes.append(shortest)
+
+        first_time = times[0]
+        other_time = np.mean(times[1:])
+        results = [n_groups, rows, columns, first_time, other_time]
+        results_full.append(results)
+        writer.writerow(results)
+
+
+
+timings_df = pd.read_csv('output/timings.csv')
+
+timings_df.plot(x='n_groups',y=['first_time','avg_time'])
+plt.xlabel('Number of nodes per cluster')
+plt.ylabel('Time taken (s)')
+plt.show()
--- network_construction.py
+++ network_construction.py
@@ -4,9 +4,9 @@ import numpy as np
 
 # Constructs a network where weights are randomly chosen from a beta distribution (most nodes are nearer)
 # Each cluster of nodes has the same number of samples in it
-def build_test_network(rows, columns, n_groups, round=2):
-    rng = np.random.default_rng(1733)
+def build_test_network(rows, columns, n_groups, round=2, seed=None):
     # subtract one from n_groups - we only need as many matrices as gaps
     alpha = 2
     beta = 5
+    rng = np.random.default_rng(seed)
     return [np.around(rng.beta(alpha, beta, size=(rows, columns)), round) for _ in range(n_groups - 1)]
